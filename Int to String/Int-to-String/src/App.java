fa6b4c009eba58734e3145371395269e35a7b3bb
Nima Shafie <nimzshafie@gmail.com>
2021-06-26 12:37:38 -0700

    test "hi" message output

--- Int to String/Int-to-String/src/App.java
+++ Int to String/Int-to-String/src/App.java
@@ -1,5 +1,10 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("hi!");
     }
 }
