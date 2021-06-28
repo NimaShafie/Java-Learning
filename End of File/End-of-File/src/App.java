a08dfc7cef79c0ba0033c9bcdc96e1667a00adac
Nima Shafie <nimzshafie@gmail.com>
2021-06-28 15:29:23 -0700

    Finished

--- End of File/End-of-File/src/App.java
+++ End of File/End-of-File/src/App.java
@@ -4,7 +4,11 @@ import java.util.*;
 class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        System.out.println("Hi!");
+        Scanner scan = new Scanner(System.in);
+        int i = 0;
+        while(scan.hasNext()){
+            i++;
+            System.out.println(i + " " + scan.nextLine());
+        }
+     }
     }
-}
